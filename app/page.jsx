7c175ca3317fa72f24250012f31a4e10a26bab8c
Sassy2312 <shashwatdk00@gmail.com>
2025-04-08 11:23:02 +0530

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+// app/page.jsx
+export default function Home() {
+  return (
+    <main style={{ color: 'white', background: 'black', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <h1>Sector Analysis Dashboard</h1>
+    </main>
+  );
+}
